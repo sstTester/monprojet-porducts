f53bdaed748b78899f2a5a660ce0ad7334565658
SstTester <kael.kucx@gmail.com>
2018-04-04 14:27:30 +0100

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Route} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,18 +8,30 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductService } from './services/product.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
+
+const routes: Route[] = [
+   {path: 'welcome', component: WelcomeComponent},
+   {path: '', component: WelcomeComponent, pathMatch: 'full'},
+   {path: 'list', component: ProductListComponent},
+   {path: '**', component: NotfoundComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductListComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
